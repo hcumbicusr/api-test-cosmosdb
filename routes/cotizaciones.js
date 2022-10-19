949a4eb3c0407027ff4eb5d2afa574323f2ea8df
hcumbicusr <hcumbicusr@gmail.com>
2022-10-19 00:41:09 -0500

    consulta id persona en bd mongo

--- routes/cotizaciones.js
+++ routes/cotizaciones.js
@@ -1,177 +1,88 @@
-var express = require('express');
+const express = require('express');
 const pool = require('../dbase/connection');
-const fetch = require("node-fetch");
-const { urlpersona } = require('../config');
-const { urldrive } = require('../config');
-//Para postgres
-//const poolPG = require('../dbase/Postgresconnection');
+// const fetch = require("node-fetch");
+// const { urlpersona } = require('../config');
+// const { urldrive } = require('../config');
 //Para Mongo
-//const poolMG = require('../dbase/Mongoonnection');
-//var mongo_persona = poolMG.get('persona_documento');
+const poolMG = require('../dbase/Mongoconnection');
 
-var router = express.Router();
+const router = express.Router();
 
 router.get('/:fechainicio/:fechafin/', async function (req, res, next) {
-  var fechainicio = req.params.fechainicio;
-  var fechafin = req.params.fechafin;
+  const fechainicio = req.params.fechainicio;
+  const fechafin = req.params.fechafin;
 
   // Buscar las cotizaciones en ADN - MYSQL
-  var [resultsADN] = await pool.query("SELECT " +
-    "numero_cotizacion," +
-    "case p.tipo_documento when 1 then 'DNI' when 2 then 'CARNET DE EXTRANJERIA' END AS tipo_documento, " +
-    "p.numero_documento, " +
-    "CONCAT('EN.',numero_cotizacion,'.',p.numero_documento,'.PDF') AS nombrearchivo, " +
-    "'' AS idpersona " +
-    "FROM siv_db.solicitud  s " +
-    "LEFT JOIN persona p ON s.id_asegurado=p.id_persona " +
-    "WHERE " +
-    "s.fecha_crea>= '" + fechainicio + " 00:00:00' AND s.fecha_crea<= '" + fechafin + " 23:59:59' " + //busqueda por fecha de cotizacion
-    "AND fecha_solicitud is not NULL;"); // cotizacion con solicitud finalizada
+  let query = `SELECT 
+      numero_cotizacion,
+      case p.tipo_documento 
+      when 1 then 'DNI' when 2 then 'CARNET DE EXTRANJERIA' END AS tipo_documento, p.numero_documento, 
+        CONCAT('EN_',numero_cotizacion,'_',p.numero_documento,'.PDF') AS nombrearchivo, '' AS idpersona 
+    FROM siv_db.solicitud  s 
+    INNER JOIN siv_db.solicitud_producto sp on s.id_solicitud = sp.id_solicitud
+    LEFT JOIN persona p ON s.id_asegurado = p.id_persona 
+    WHERE str_to_date(sp.fecha_cotizacion, '%d/%m/%Y') >= '${fechainicio} 00:00:00' AND str_to_date(sp.fecha_cotizacion, '%d/%m/%Y')  <= '${fechafin} 23:59:59';`;
+  console.log("Query", query);
+  const [resultsADN] = await pool.query(query); // cotizacion con solicitud finalizada
 
-  var cotizaciones = [];
-  var personas = [];
+  let cotizaciones = [];
+  let personas = [];
+  let personas_buscar = [];
 
   console.log('Se encontraron ' + resultsADN.length + ' registros')
-  var i=0;
+  let i=0;
   for (const elementADN of resultsADN) {
     i=i+1;
-    console.log('Registro ' + i + ' de '+ resultsADN.length)
-    var data = { tipo_documento: elementADN.tipo_documento, numero_documento: elementADN.numero_documento };//43307114
-    const optionsp = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json; charset=utf-8'
-      },
-      body: JSON.stringify(data)
-    };
-    // Buscar ID Persona en MongoDB
-    let datap = await fetch(urlpersona, optionsp);
-    //console.log('urlpersona')    
-    //console.log(urlpersona)
-    //console.log('optionsp')
-    //console.log(optionsp)
-    //console.log('datap')
-    //console.log(datap)
-    const personasmongo = await datap.json();
-    //console.log('personasmongo')
-    //console.log(personasmongo)
+    console.log(i + '/'+ resultsADN.length+': ', elementADN.numero_cotizacion, elementADN.tipo_documento, elementADN.numero_documento);
     
-    for (const element of personasmongo.data) {
-
-      personas.push({
-        tipo_documento: elementADN.tipo_documento, num_documento: elementADN.numero_documento,
-        nombrearchivo: elementADN.nombrearchivo, idpersona: element.ID_PERSONA.toLowerCase()
-      });
-      //console.log('personas')
-      //console.log(personas)
-      const options = {
-        method: 'GET',
-        headers: {
-          'app': 'CRM'
-        }
-      };
-      // Buscar Archivos Drive para ID Persona
-      let datad = await fetch(urldrive + element.ID_PERSONA.toLowerCase(), options)
-      //console.log('datad')
-      //console.log(datad)
-      var contactid;
-      const drivedata = await datad.json();
-      if (drivedata.length > 0) {
-        contactid = element.ID_PERSONA.toLowerCase();
-      } else { contactid = "" }
-
-    }
-    //Lógica referencia ADN uploadArchivo
-    var contactVal = "";
-    if (contactid != "") { // si tiene archivos drive se toma el idpersona
-      contactVal = contactid;
-    } else {
-      if (personas.length > 1) { // si not tiene archivos drive
-        contactVal = personas[1].idpersona;  // si es más de 1 idpersona se toma el segundo
-      } else {
-        contactVal = personas[0].idpersona; // si solo hay 1 idpersona se toma el primero
-      }
-    }
     cotizaciones.push({
       num_solicitud: elementADN.numero_cotizacion,
-      tipo_documento: elementADN.tipo_documento, num_documento: elementADN.numero_documento,
-      ruta_cloud_storage: contactVal + '/' + elementADN.nombrearchivo
+      tipo_documento: elementADN.tipo_documento, 
+      num_documento: elementADN.numero_documento,
+      ruta_cloud_storage: elementADN.nombrearchivo
     });
 
+    const data = { "TIPO_DOCUMENTO": elementADN.tipo_documento, "NUMERO_DOCUMENTO": elementADN.numero_documento };
+    if ( personas.includes(elementADN.tipo_documento+"-"+elementADN.numero_documento) ) continue;
+    personas.push(elementADN.tipo_documento+"-"+elementADN.numero_documento);
+    personas_buscar.push(data);
   }
-  /*
-  // ejemplo consumir funcion getPersona
-  async function buscapersona() {
-    console.log('resultsADN');
-    console.log(resultsADN);
-    for (const element of resultsADN) {
-      const personas = await getPersona(element.tipo_documento, element.numero_documento);
-      cotizaciones.push({
-        tipo_documento: element.tipo_documento, num_documento: element.numero_documento,
-        nombrearchivo: element.nombrearchivo, idpersona: personas.ID_PERSONA.toLowerCase()
-      });
-    }
-   return cotizaciones;
-  }
-  res.json(await buscapersona());
-  */
 
+  const personasmongo = await getPersonas(personas_buscar);
+  console.log("personasmongo", personasmongo);
+
+  for (let cotizacion of cotizaciones) {
+    const personanomgo = personasmongo.find(function(element) {
+      if (element.TIPO_DOCUMENTO == cotizacion.tipo_documento && element.NUMERO_DOCUMENTO == cotizacion.num_documento) return element;
+      return null;
+    });
+    if ( personanomgo.ID_PERSONA ) {
+      const ID_PERSONA = personanomgo.ID_PERSONA.toLowerCase();
+      console.log(`ID_PERSONA[${cotizacion.tipo_documento}-${cotizacion.num_documento}]:`, ID_PERSONA);
+      cotizacion.ruta_cloud_storage = ID_PERSONA + '/' + cotizacion.ruta_cloud_storage;
+    }
+  }
   res.json(cotizaciones);
 });
 
-/*
-// ejemplo de funcion
+// Siempre retorna 1 solo objeto
 async function getPersona(tipodoc, numdoc) {
-  //console.log('ingreso getpersona' + tipodoc + numdoc);
-  var query = {
+  let query = {
     TIPO_DOCUMENTO: tipodoc,
     NUMERO_DOCUMENTO: numdoc
   };
-  var [results] = await mongo_persona.find(query);
+  let mongo_persona = poolMG.get('persona_documento');
+  let [results] = await mongo_persona.find(query);
   return results;
 }
 
-
-
-//POSTGRES
-router.get('/:id', async function (req, res, next) {
-  try {
-    var id = req.params.id;
-    console.log('ingreso PG');
-    //const data =  await poolPG.query('select * from cvw_cot c where c.id = $1', [id]);
-    const data = await poolPG.query('select * from cvw_cot c limit 1');
-    console.log('data');
-    console.log(data);
-    let cotizacion = null;
-    if (data.length > 0) {
-      cotizacion = data[0];
-    }
-    //return cotizacion;
-    res.json(cotizacion)
-  } catch (error) {
-    console.error(error);
-  }
-});
-
-
-
-//MONGO
-router.get('/mongodb/:num_documento', async function (req, res, next) {
-  console.log('Ingreso MONGO');
-  var num_documento = req.params.num_documento;
-  var query = {
-    TIPO_DOCUMENTO: 'DNI',
-    NUMERO_DOCUMENTO: num_documento
-  };
-  console.log(query);
-  mongo_persona.find(query)
-    .then(results => {
-      console.log(results);
-      res.json(results);
-    })
-    .catch(error => console.error(error));
-});
-*/
-
+async function getPersonas(buscar) {
+  let query = {$or: buscar};
+  console.log("query", query)
+  let mongo_persona = poolMG.get('persona_documento');
+  let results = await mongo_persona.find(query);
+  console.log("results", results);
+  return results;
+}
 
 module.exports = router;
