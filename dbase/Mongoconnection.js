440d5816f5c61e71906a6e862cf65190c93c977c
hcumbicusr <hcumbicusr@gmail.com>
2022-12-28 22:33:32 -0500

    usar nuevos servidores mongo cloud

--- dbase/Mongoconnection.js
+++ dbase/Mongoconnection.js
@@ -1,10 +1,15 @@
 const monk = require('monk');
 const {mongodb_host, mongodb_name, mongodb_port, mongodb_username, mongodb_password} = require('../config');
 let db = null;
-if ( mongodb_username && mongodb_password) {
-    db = monk(`${mongodb_username}:${mongodb_password}@${mongodb_host}:${mongodb_port}/${mongodb_name}`);
+if (process.env.EDN_MONGO_REPLICA=='true') {
+    console.log("mongo set replica user+pass: ", `mongodb://${mongodb_username}:****@${mongodb_host}/${mongodb_name}?replicaSet=rs0`);
+    db = monk(`mongodb://${mongodb_username}:${mongodb_password}@${mongodb_host}/${mongodb_name}`);
+} else if ( mongodb_username && mongodb_password) {
+    console.log("mongo set user+pass: ", `mongodb://${mongodb_username}:****@${mongodb_host}:${mongodb_port}/${mongodb_name}`);
+    db = monk(`mongodb://${mongodb_username}:${mongodb_password}@${mongodb_host}:${mongodb_port}/${mongodb_name}`);
 } else {
-    db = monk(`${mongodb_host}:${mongodb_port}/${mongodb_name}`);
+    console.log("mongo: ", `mongodb://${mongodb_host}:${mongodb_port}/${mongodb_name}`);
+    db = monk(`mongodb://${mongodb_host}:${mongodb_port}/${mongodb_name}`);
 }
 
 module.exports = db;
